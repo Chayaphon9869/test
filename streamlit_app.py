8da316f7734d0ce88da0881d52564f82935dd591
Chayaphon9869 <tuxidowai98@gmail.com>
2024-08-03 16:34:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 st.title('Data Display with Streamlit')
 
 # Load data
-file_path = 'E:\\Project\\automationpatch\\test.csv'  # Replace with your file path
+file_path = 'test.csv'  # Replace with your file path
 data = pd.read_csv(file_path)  # Use read_csv for CSV files
 
 # Display the data
